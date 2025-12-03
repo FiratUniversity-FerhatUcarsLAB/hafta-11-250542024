c196581312df91bf4380b1c191b7894c6ae213df
250542024 <250542024@firat.edu.tr>
2025-12-03 20:18:00 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,21 +1,21 @@
 public class Exercise4_2 {
 
-    public static void main(String[] args) {              // 1
-        zippo("rattle", 13);                              // 2
+    public static void main(String[] args) {          // 1
+        zippo("rattle", 13);                          // 2
     }
 
-    public static void baffle(String blimp) {              // 5
-        System.out.println(blimp);                        // 6
-        zippo("ping", -5);                                // 7
+    public static void baffle(String blimp) {         // 5
+        System.out.println(blimp);                    // 6
+        zippo("ping", -5);                            // 7
     }
 
-    public static void zippo(String quince, int flag) {   // 3 (ilk çağrıda)
-        if (flag < 0) {                                   // 8 (ikinci çağrıda kontrol)
-            System.out.println(quince + " zoop");         // 9
+    public static void zippo(String quince, int flag) {
+        if (flag < 0) {                               // 8
+            System.out.println(quince + " zoop");     // 9
         } else {
-            System.out.println("ik");                     // 4
-            baffle(quince);                               // 5 (çağrı)
-            System.out.println("boo-wa-ha-ha");           // 10
+            System.out.println("ik");                 // 3
+            baffle(quince);                           // 4
+            System.out.println("boo-wa-ha-ha");       // 10
         }
     }
 }
