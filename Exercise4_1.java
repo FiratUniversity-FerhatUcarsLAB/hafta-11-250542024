5aee85fc9f019ed39af4e71c331456bff7ffe1a9
250542024 <250542024@firat.edu.tr>
2025-12-03 20:10:48 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,23 @@
 public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        System.out.println("American format: " + day + ", " + month + " " + date + ", " + year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        System.out.println("European format: " + day + " " + date + " " + month + " " + year);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+
+        // Örnek tarih bilgileri
+        String day = "Monday";
+        int date = 18;
+        String month = "November";
+        int year = 2025;
+
+        // Metotları test et
+        printAmerican(day, date, month, year);
+        printEuropean(day, date, month, year);
     }
 }
