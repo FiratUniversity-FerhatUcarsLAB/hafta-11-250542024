5654acc21633716554998247f8d2c2efcd40818b
250542024 <250542024@firat.edu.tr>
2025-12-03 20:12:58 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,21 @@
 public class Exercise4_2 {
 
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
-
-    public static void main(String[] args) {
-        zippo("rattle", 13);
+    public static void main(String[] args) {              // 1
+        zippo("rattle", 13);                              // 2
     }
 
-    public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+    public static void baffle(String blimp) {              // 5
+        System.out.println(blimp);                        // 6
+        zippo("ping", -5);                                // 7
     }
 
-    public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
+    public static void zippo(String quince, int flag) {   // 3 (ilk çağrıda)
+        if (flag < 0) {                                   // 8 (ikinci çağrıda kontrol)
+            System.out.println(quince + " zoop");         // 9
         } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+            System.out.println("ik");                     // 4
+            baffle(quince);                               // 5 (çağrı)
+            System.out.println("boo-wa-ha-ha");           // 10
         }
     }
 }
