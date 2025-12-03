92f4ac8efe911c16f15fa7bd55cfe9930e4ca041
250542024 <250542024@firat.edu.tr>
2025-12-03 20:15:33 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,22 +1,24 @@
 public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
-        System.out.println("American format: " + day + ", " + month + " " + date + ", " + year);
+        // American Format: Monday, July 22, 2019
+        System.out.println("American Format");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        System.out.println("European format: " + day + " " + date + " " + month + " " + year);
+        // European Format: 22 July 2019, Monday
+        System.out.println("European Format");
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
 
     public static void main(String[] args) {
 
-        // Örnek tarih bilgileri
         String day = "Monday";
-        int date = 18;
-        String month = "November";
-        int year = 2025;
+        String month = "July";
+        int date = 22;
+        int year = 2019;
 
-        // Metotları test et
         printAmerican(day, date, month, year);
         printEuropean(day, date, month, year);
     }
